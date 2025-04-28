86244eab94224c2467f20053586c3ec34d06c6cc
Efe Awo-Osagie <deafveid@gmail.com>
2025-04-28 11:26:05 -0500

    First modifications made to football, team, and player service

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlayerService {
-
   constructor() { }
+
+  footballPlayers: any[] = [
+    {
+      sport: "football", team: "NDSU Bisons", name: "Player 1",
+      age: "20", hometown: "Minot, ND", height: "7' 9",
+      weight: "245", prevTeam: "Sike!", yrJoined: "2022", academic: "Earth Bsc"
+    },
+    {sport: "football", team: "NDSU Bisons", name: "Player 2"},
+    {sport: "football", team: "NDSU Bisons", name: "Player 3"},
+    {sport: "football", team: "NDSU Bisons", name: "Player 4"},
+    {sport: "football", team: "NDSU Bisons", name: "Player 5"},
+    {sport: "football", team: "Cherries", name: "Player 1"},
+    {sport: "football", team: "Cherries", name: "Player 2"},
+    {sport: "football", team: "Cherries", name: "Player 3"},
+    {sport: "football", team: "Cherries", name: "Player 4"},
+    {sport: "football", team: "Cherries", name: "Player 5"},
+    {sport: "football", team: "Kangaroos", name: "Player 1"},
+    {sport: "football", team: "Kangaroos", name: "Player 2"},
+    {sport: "football", team: "Kangaroos", name: "Player 3"},
+    {sport: "football", team: "Kangaroos", name: "Player 4"},
+    {sport: "football", team: "Kangaroos", name: "Player 5"},
+  ];
 }
--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -3,7 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class TeamService {
 
+export class TeamService {
   constructor() { }
+
+  footballTeams: any[] = [
+    {name: "NDSU Bisons", school: "NDSU", manager: "Tom Braidy", coachStaff: ["Caleb", "Johnie", "Adam", "Efe"]},
+    {name: "Cherries", school: "UND", manager: "Joel Imposter", coachStaff: ["Belac", "Einhoj", "Mada", "Efe"]},
+    {name: "Kangaroos", school: "MSU", manager: "Tim Drinker", coachStaff: ["Mustafi", "Chill", "Fake", "Dennis"]},
+    {name: "Tin Cans", school: "SDSU", manager: "Cavinger Haus", coachStaff: ["Belac", "Einhoj", "Mada", "Tommy"]},
+    {name: "Ballers", school: "BSC", manager: "Jocob Sprit", coachStaff: ["Saka", "Chill", "Haaland", "Cristiano"]},
+  ]
 }
