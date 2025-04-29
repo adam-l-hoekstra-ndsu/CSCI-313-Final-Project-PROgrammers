e19b5e2df98f85ceddeb79b523721adfa01c1110
adam-l-hoekstra-ndsu <adam.l.hoekstra@ndsu.edu>
2025-04-29 10:29:12 -0500

    WIP commit for game-play-entry component

--- src/app/game-play-entry/game-play-entry.component.ts
+++ src/app/game-play-entry/game-play-entry.component.ts
@@ -9,10 +9,12 @@ import { Match } from '../match';
 import { MatchService } from '../match.service';
 import { GameStatisticsComponent } from '../game-statistics/game-statistics.component';
 import { SecondsToTimePipe } from '../seconds-to-time.pipe';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-game-play-entry',
-  imports: [SecondsToTimePipe],
+  imports: [SecondsToTimePipe, FormsModule],
   templateUrl: './game-play-entry.component.html',
   styleUrl: './game-play-entry.component.css'
 })
@@ -22,6 +24,15 @@ export class GamePlayEntryComponent {
   
   team1!: Team;
   team2!: Team;
+
+  playerActing!: Player;
+  playerEffected!: Player;
+  playerAssisting!: Player;
+  playTimeMinutes!: number;
+  playTimeSeconds!: number;
+  playTime!: number;
+  playOvertime!: boolean;
+  playDescription!: string;
 
   sportEnum = Sport; // For use in the template
 
@@ -36,6 +47,37 @@ export class GamePlayEntryComponent {
     this.team1 = this.teamService.getTeam(this.match.team1ID);
     this.team2 = this.teamService.getTeam(this.match.team2ID);
   }
+
+  playDescriptionBuilderAssist(playerActing: Player, playerEffected: Player, playerAssisting: Player, playAction: string): string {
+    let description = playerActing.firstName + " " + playerActing.lastName;
+    description += playAction + " " + playerEffected.firstName + " " + playerEffected.lastName;
+    description += "assisted by " + playerAssisting.firstName + " " + playerAssisting.lastName;
+    return description;
+  }
+
+  playDescriptionBuilder(playerActing: Player, playerEffected: Player, playAction: string): string {
+    let description = playerActing.firstName + " " + playerActing.lastName;
+    description += playAction + " " + playerEffected.firstName + "" + playerEffected.lastName;
+    return description;
+  }
+
+  onSubmit(form: NgForm) {
+    this.playTime = (Number(this.playTimeMinutes * 60) + Number(this.playTimeSeconds));
+    if (this.playOvertime) {this.playTime = -this.playTime;} // - denotes overtime
+    if(this.playerAssisting == null) {
+      this.playDescription = this.playDescriptionBuilderAssist(this.playerActing, this.playerEffected, this.playerAssisting, this.playDescription);
+    }
+    else {
+      this.playDescription = this.playDescriptionBuilder(this.playerActing, this.playerEffected, this.playDescription);
+    }
+    let play: Play = {
+      time: this.playTime,
+      description: this.playDescription,
+    };
+    this.matchService.addPlayToMatch(this.match, play);
+    console.log("Play added to match: ", play);
+  }
+
 }
 
 
--- src/app/match.service.ts
+++ src/app/match.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Match } from './match';
 import { matches } from './match-data';
+import { Play } from './play';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
+
+  addPlayToMatch(match: Match, play: Play) {
+    match.quarterOrRoundResults[match.quarterOrRound].plays.push(play);
+  }
 
   matches = matches;
 
