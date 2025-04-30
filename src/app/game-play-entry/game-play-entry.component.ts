b6838fb59a6d553ff17e1f97723fce827eee3986
Efe Awo-Osagie <deafveid@gmail.com>
2025-04-30 10:45:41 -0500

    Adam (#10)
    * Changed underlying data structures and substantially improved the game-view and game-statistic components.
    
    * Updated game-view component to dynamically show certain content bassed on if the match has started or not.
    
    * Changed the score by quarter HTML to better handle cases where a game goes into overtime.
    
    * WIP commit for game-play-entry component
    
    * Fixed game-statistics column labels for name
    
    * Added basic functionality to game-play-entry component
    
    ---------
    
    Co-authored-by: adam-l-hoekstra-ndsu <adam.l.hoekstra@ndsu.edu>

--- src/app/game-play-entry/game-play-entry.component.ts
+++ src/app/game-play-entry/game-play-entry.component.ts
@@ -1,11 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RainbowSixSiegeService, SiegeCatagory } from '../rainbow-six-siege.service';
+import { Sport } from '../sport';
+import { Player } from '../player';
+import { Team } from '../team'
+import { Play } from '../play';
+import { TeamService } from '../team.service';
+import { Match } from '../match';
+import { MatchService } from '../match.service';
+import { GameStatisticsComponent } from '../game-statistics/game-statistics.component';
+import { SecondsToTimePipe } from '../seconds-to-time.pipe';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-game-play-entry',
-  imports: [],
+  imports: [SecondsToTimePipe, FormsModule, GameStatisticsComponent],
   templateUrl: './game-play-entry.component.html',
   styleUrl: './game-play-entry.component.css'
 })
 export class GamePlayEntryComponent {
+  matchID: number = 3; // Hardcoding match id for now
+  match!: Match;
+  
+  team1!: Team;
+  team2!: Team;
+
+  playerActing!: Player;
+  playerEffected!: Player;
+  playerAssisting!: Player;
+  playTimeMinutes!: number;
+  playTimeSeconds!: number;
+  playTime!: number;
+  playOvertime!: boolean;
+  playDescription!: string;
+  action!: string;
+
+  sportEnum = Sport; // For use in the template
+
+  constructor() {}
+
+  siegeService = inject(RainbowSixSiegeService);
+  teamService = inject(TeamService);
+  matchService = inject(MatchService)
+
+  ngOnInit(): void {
+    this.match = this.matchService.getMatchById(this.matchID);
+    this.team1 = this.teamService.getTeam(this.match.team1ID);
+    this.team2 = this.teamService.getTeam(this.match.team2ID);
+  }
+
+  setAction(action : string) {
+    this.action = action;
+  }
+
+  playDescriptionBuilderAssist(playerActing: Player, playerEffected: Player, playerAssisting: Player, playAction: string): string {
+    let description = this.playDescriptionBuilder(playerActing, playerEffected, playAction);
+    description += ", assisted by " + playerAssisting.firstName + " " + playerAssisting.lastName;
+    return description;
+  }
+
+  playDescriptionBuilder(playerActing: Player, playerEffected: Player, playAction: string): string {
+    let description = playerActing.firstName + " " + playerActing.lastName;
+    description +=  " " + playAction + " " + playerEffected.firstName + " " + playerEffected.lastName;
+    return description;
+  }
+
+  onSubmit(form: NgForm) {
+    this.playTime = (Number(this.playTimeMinutes * 60) + Number(this.playTimeSeconds));
+    if (this.playOvertime) {this.playTime = -this.playTime;} // - denotes overtime
+
+    // Handle Assists and Play Description
+    if(this.playerAssisting != null) {
+      this.playDescription = this.playDescriptionBuilderAssist(this.playerActing, this.playerEffected, this.playerAssisting, this.action);
+      this.playerAssisting.stats[this.matchID][SiegeCatagory.Assists]++;
+    }
+    else {
+      this.playDescription = this.playDescriptionBuilder(this.playerActing, this.playerEffected, this.action);
+    }
+
+    // Handle Kills
+    if(this.action == 'Killed') {
+      this.playerActing.stats[this.matchID][SiegeCatagory.Kills]++;
+      this.playerEffected.stats[this.matchID][SiegeCatagory.Deaths]++;
+    }
+
+    // Handle Revives
+    if(this.action == 'Revived') {
+      this.playerActing.stats[this.matchID][SiegeCatagory.Revives]++;
+    }
+
+    // Handle Objective Plays
+    if(this.action == 'Planted/Disabled the Defuser') {
+      this.playerActing.stats[this.matchID][SiegeCatagory.ObjectivePlays]++;
+    }
+
+    // Create and add the play object
+    let play: Play = {
+      time: this.playTime,
+      description: this.playDescription,
+    };
+    this.matchService.addPlayToMatch(this.match, play);
+  }
 
 }
+
+
--- src/app/game-statistics/game-statistics.component.ts
+++ src/app/game-statistics/game-statistics.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Input } from '@angular/core';
 import { RainbowSixSiegeService } from '../rainbow-six-siege.service';
-import { Sport } from '../sport';
-import { Player } from '../player';
+import { FootballService } from '../football.service';
+import { VolleyballService } from '../volleyball.service';
+import { BasketballService } from '../basketball.service';
 import { Team } from '../team'
 import { TeamService } from '../team.service';
 import { Match } from '../match';
 import { MatchService } from '../match.service';
+import { Sport } from '../sport';
 
 @Component({
   selector: 'app-game-statistics',
@@ -15,31 +17,46 @@ import { MatchService } from '../match.service';
 })
 export class GameStatisticsComponent implements OnInit {
   categories: string[] = [];
-  sport: Sport = Sport.RainbowSixSiege; // Hardcoding sport to Rainbow Six Siege for now
 
-  matchID: number = 3; // Hardcoding match id to 3 for now
+  @Input() matchID!: number;
   match!: Match;
   
-  team1ID: number = 1; // Hardcoding for now
   team1!: Team;
-  team2ID: number = 2; // Hardcoding for now
   team2!: Team;
-  team1Players: Player[] = [];
-  team2Players: Player[] = [];
 
   constructor() {}
 
   siegeService = inject(RainbowSixSiegeService);
+  footballService = inject(FootballService);
+  volleyballService = inject(VolleyballService);
+  basketballService = inject(BasketballService);
   teamService = inject(TeamService);
   matchService = inject(MatchService)
 
   ngOnInit(): void {
-    this.categories = this.siegeService.statCategories;
-    this.team1 = this.teamService.getTeam(this.team1ID);
-    this.team2 = this.teamService.getTeam(this.team2ID);
-    this.team1Players = this.teamService.getPlayers(this.team1);
-    this.team2Players = this.teamService.getPlayers(this.team2);
     this.match = this.matchService.getMatchById(this.matchID);
+    this.team1 = this.teamService.getTeam(this.match.team1ID);
+    this.team2 = this.teamService.getTeam(this.match.team2ID);
+    this.setCategories(this.team1.sport);
+  }
+
+  setCategories(sport: Sport) {
+    switch (sport) {
+      case Sport.RainbowSixSiege:
+        this.categories = this.siegeService.statCategories;
+        break;
+      // case Sport.Football:
+      //   this.categories = this.footballService.statCategories;
+      //   break;
+      // case Sport.Volleyball:
+      //   this.categories = this.volleyballService.statCategories;
+      //   break;
+      // case Sport.Basketball:
+      //   this.categories = this.basketballService.statCategories;
+      //   break;
+      default:
+        this.categories = [];
+    }
   }
 
 
