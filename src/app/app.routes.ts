57720788d64d347ac6bbef3d7818125386c82353
Efe Awo-Osagie <deafveid@gmail.com>
2025-04-30 09:51:13 -0500

    Created login a page, updated admin page, and added routing to login page and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SportSelectionComponent } from './sport-selection/sport-selection.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: SportSelectionComponent,
+        title: "Bison Base"
+    },
+    {
+        path: "login",
+        component: LoginComponent,
+        title: "Admin"
+    }
+];
