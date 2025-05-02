efd0550aea25f2ec1c049b0713a986c351710374
jrzaharia <johnnie.zaharia@ndsu.edu>
2025-05-02 01:46:50 -0500

    Removed unused imports from App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { GameStatisticsComponent } from './game-statistics/game-statistics.component';
-import { SportSelectionComponent } from './sport-selection/sport-selection.component';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    GameStatisticsComponent,
-    SportSelectionComponent,
-    HomeComponent,
-  ],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
