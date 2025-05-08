a2ea6c7d4288136b8f85310bc20607cdb637bef2
adam-l-hoekstra-ndsu <adam.l.hoekstra@ndsu.edu>
2025-05-08 09:07:54 -0500

    Reverted team, player, and match services due to issues with game pages

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -2,41 +2,28 @@ import { Injectable, OnInit, inject } from '@angular/core';
 import { Team } from './team';
 import { Player } from './player';
 import { PlayerService } from './player.service';
-import {
-  collection,
-  Firestore,
-  collectionData,
-  doc,
-  setDoc,
-  DocumentReference,
-  updateDoc,
-  deleteDoc,
-  docData,
-} from '@angular/fire/firestore';
+import { collection, Firestore, collectionData, doc, setDoc, DocumentReference, updateDoc, deleteDoc, docData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { SportInfo, SportsService } from './sports.service';
 import { Sport } from './sport';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 
-export class TeamService  {
-
-  teams: Team[] = []; //Note: This is a local SNAPSHOT of the firebase databse. Changes made to this array WILL NOT be updated on the Database
+export class TeamService implements OnInit {
+  
+  teams: Team[] = [] //Note: This is a local SNAPSHOT of the firebase databse. Changes made to this array WILL NOT be updated on the Database
 
   playerService = inject(PlayerService);
 
-  constructor() {
-    this.getTeams().subscribe((data) => (this.teams = data));
-    console.log(this.teams);
-  }
+  constructor() { }
 
   private firestore = inject(Firestore);
   teamCollection = collection(this.firestore, 'team-data');
 
   ngOnInit() {
-    this.getTeams().subscribe((data) => (this.teams = data));
+    this.getTeams().subscribe(data => this.teams = data);
     console.log(this.teams);
   }
 
@@ -50,21 +37,21 @@ export class TeamService  {
     return docData(teamDocRef, { idField: 'id' }) as Observable<Team>;
   }
 
-  getTeams(): Observable<Team[]> {
-    return collectionData(this.teamCollection, { idField: 'id' }) as Observable<Team[]>;
+  getTeams(): Observable<Team[]>{
+    return collectionData(this.teamCollection, ({idField: 'id'})) as Observable<Team[]>;
   }
 
-  addTeam(newTeam: Team) {
+  addTeam(newTeam: Team){
     const teamRef = doc(this.teamCollection);
     const newId = teamRef.id;
     newTeam.id = newId;
     setDoc(teamRef, newTeam);
   }
 
-  updateTeam(id: string, team: Partial<Team>): Promise<void> {
-    console.log('called ' + 'new name: ' + team.name);
+  updateTeam(id: string, team : Partial<Team>): Promise<void> {
+    console.log("called "+"new name: "+team.name)
     const teamDoc = doc(this.firestore, `team-data/${id}`);
-    return updateDoc(teamDoc, { ...team });
+    return updateDoc(teamDoc, {... team});
   }
 
   deleteTeam(id: string): Promise<void> {
@@ -73,25 +60,25 @@ export class TeamService  {
   }
 
   // This is the method used to build each sport in the sport-selection component
+  
 
   //This is the method used to create a dataset of teams based on the sport selected and save it as an array in the sport-home component.
   getSportTeams(sport: Sport): Team[] {
-    return this.teams.filter((team) => team.sport == sport);
+    return this.teams.filter(team => team.sport == sport);
   }
 
   //This is the method used to create a dataset of information relating to the selected sport (e.g. sport name and sport picture)
-  //and then save it as an array in the sport-home component
-
-  //this is the method used to generate all the teams for the sport selected in the sport-selection component
-  genSportTeams(sport: Sport): Team[] {
-    return this.teams.filter((team) => team.sport == sport);
-  }
   //and then save it as an array in the sport-home component 
   
 
-  createTeam(named: string, sports: Sport, logo: string, league: string) {
-    let t: Team = {
-      id: '',
+  //this is the method used to generate all the teams for the sport selected in the sport-selection component
+  genSportTeams(sport: Sport): Team[] {
+    return this.teams.filter(team => team.sport == sport);
+  }
+
+  createTeam(named: string, sports: Sport, logo:string, league:string){
+    let t:Team = {
+      id: "",
       name: named,
       sport: sports,
       logoUrl: logo,
@@ -100,30 +87,30 @@ export class TeamService  {
       wins: 0,
       losses: 0,
       draws: 0,
-      leagueSubsection: league,
-    };
+      leagueSubsection: league
+    }
     this.addTeam(t);
   }
 
-  editTeam(t: Team, name: string, league: string, logo: string) {
-    t.name = name;
-    t.leagueSubsection = league;
-    t.logoUrl = logo;
-    this.updateTeam(t.id, t);
+  editTeam(t:Team, name: string, league: string, logo: string) {
+    t.name = name
+    t.leagueSubsection = league
+    t.logoUrl = logo
+    this.updateTeam(t.id, t)
   }
 
   getPlayerIDs(teamId: string): string[] {
-    return this.teams.filter((team) => team.id == teamId)[0].players;
+    return this.teams.filter(team => team.id == teamId)[0].players;
   }
 
-  playerLeave(playerToRemove: Player, team: Team) {
+  playerLeave(playerToRemove: Player, team:Team) {
     // const teamSpec: Team = this.getTeam(team) *team argument is already a Team object
     // const index = team.players.indexOf(player)
     // teamSpec.players.splice(index,1)
-    team.players = team.players.filter((player) => player != playerToRemove.id);
+    team.players = team.players.filter(player => player != playerToRemove.id);
   }
 
-  playerJoin(plr: Player, team: Team) {
+  playerJoin(plr :Player, team: Team) {
     // const teamSpec: Team = this.getTeam(team.id)
     // const index = teamSpec.players.indexOf(plr.id,0)
     // teamSpec.players.push(plr.id)
